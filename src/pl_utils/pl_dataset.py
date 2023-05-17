8a8b519ac2bb6050305958d2eacbff2870cb7add
TheoLisin <theo.lisin@gmail.com>
2023-05-17 21:06:34 +0000

    upd pl dataset: add TranlationDataset support

--- src/pl_utils/pl_dataset.py
+++ src/pl_utils/pl_dataset.py
@@ -1,22 +1,28 @@
 import pytorch_lightning as pl
-from typing import Any
+from typing import Any, Tuple
+from torch import Tensor
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 from data_utils.dataset import TranslationDataset
+from data_utils.lang import PAD
 
 
-class TranslationDataset(pl.LightningDataModule):
-    def __init__(self, train_data: Any, val_data: Any, train_bs: int, test_bs: int):
+class PlTranslationDataset(pl.LightningDataModule):
+    def __init__(
+        self,
+        train_dataset: TranslationDataset,
+        val_dataset: TranslationDataset,
+        train_bs: int,
+        test_bs: int,
+    ):
         super().__init__()
-        self.train_data = train_data
-        self.val_data = val_data
+        self.train_dataset = train_dataset
+        self.val_dataset = val_dataset
         self.train_bs = train_bs
         self.test_bs = test_bs
-
-    def setup(self, stage=None):
-        if stage == "fit" or stage is None:
-            self.train_dataset = TranslationDataset(self.train_data)
-            self.val_dataset = TranslationDataset(self.val_data)
+        self.src_pad = self.train_dataset.source_lang[PAD]
+        self.trg_pad = self.train_dataset.target_lang[PAD]
 
     def train_dataloader(self):
         return DataLoader(
@@ -24,6 +30,7 @@ class TranslationDataset(pl.LightningDataModule):
             batch_size=self.train_bs,
             shuffle=True,
             num_workers=4,
+            collate_fn=self.collate_batch,
         )
 
     def val_dataloader(self):
@@ -31,4 +38,11 @@ class TranslationDataset(pl.LightningDataModule):
             self.val_dataset,
             batch_size=self.test_bs,
             num_workers=4,
+            collate_fn=self.collate_batch,
         )
+
+    def collate_batch(self, batch: Tuple[Tensor, Tensor]):
+        src, trg = batch
+        src_pad = pad_sequence(src, padding_value=self.src_pad)
+        trg_pad = pad_sequence(trg, padding_value=self.trg_pad)
+        return src_pad, trg_pad
