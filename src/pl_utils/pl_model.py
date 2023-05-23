a7a4465288fafce46b70dfb040dde39f8859f53e
TheoLisin <theo.lisin@gmail.com>
2023-05-23 00:21:06 +0000

    kwargs to forward

--- src/pl_utils/pl_model.py
+++ src/pl_utils/pl_model.py
@@ -16,11 +16,12 @@ class ModelWrapper(pl.LightningModule):
         self.loss = criterion_fn
         self.optimizer_fn = optimizer_fn
 
-    def forward(self, data, batch_idx):
-        return self.model(data)
+    def forward(self, data, batch_idx, **kwargs) -> Tensor:
+        return self.model(data, **kwargs)
             
     def step(self, batch, batch_idx, metric, prog_bar=False):
         x, y = batch
+        batch_size = x.shape[1]
         try:
             translation = self.forward(x, batch_idx)
         except ValueError:
@@ -28,11 +29,18 @@ class ModelWrapper(pl.LightningModule):
         # print(translation.view(-1, translation.shape[-1]).shape)
         # print(y.view(-1).shape)
         loss = self.loss(translation, y)
-        self.log(metric, loss, prog_bar=prog_bar)
+        self.log(metric, loss, prog_bar=prog_bar, batch_size=batch_size)
         return loss
 
     def test_step(self, batch, batch_idx, prog_bar=False):
-        raise NotImplementedError
+        x, y = batch
+        try:
+            translation = self.forward(x, batch_idx)
+        except ValueError:
+            translation = self.forward(batch, batch_idx)
+        
+        gen = translation.argmax(dim=-1).detach().cpu().numpy()
+        return y, gen
 
     def training_step(self, batch, batch_idx):
         return self.step(batch, batch_idx, "train_loss")
